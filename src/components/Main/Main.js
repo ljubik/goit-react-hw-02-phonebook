02e76c98459960f55a7f548c9f07269595eac29f
Liubomyr <ljubik@ukr.net>
2021-03-07 12:59:45 +0200

    rebuild components

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
+import style from "../Main/Main.module.css";
 
-function Main({ db }) {
+
+function Main({ title }) {
 
   return (
     <main>
-      <h1>Hello my Project</h1>
+      <h1 className={style.center }>{title}</h1>
     </main>
   );
 }
 
 export default Main;
+
+Main.propTypes = {
+  title: PropTypes.string.isRequired,
+};
